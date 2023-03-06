e55ec69ebc56fca5d23c11434881a4abe96234cd
idasitich <36906496+idasitich@users.noreply.github.com>
2023-03-06 09:52:42 +0700

    2 files modified

--- src/layout/layout.component.ts
+++ src/layout/layout.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
 })
-export class LayoutComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class LayoutComponent {
+  bag = '../images/suits.jpeg';
 }
